b8cec748b7bd155ab5b16114a8b36d0b2bbef1ef
hkvoluntary <97347174+hkvoluntary@users.noreply.github.com>
2023-07-10 02:16:56 -0700

    Update open_weather_map.js

--- course/WeatherProject/open_weather_map.js
+++ course/WeatherProject/open_weather_map.js
@@ -1,10 +1,10 @@
-const WEATHER_API_KEY = "bbeb34ebf60ad50f7893e7440a1e2b0b";
+const WEATHER_API_KEY = "_YOU_API_KEY_";
 const API_STEM = "http://api.openweathermap.org/data/2.5/weather?";
 
 //New York zip code: 10001 10005 10021
 //Los Angles zip code: 90001 90005 90012
 //San Francisco zip code: 94104 94119
-//URL: http://api.openweathermap.org/data/2.5/weather?zip=10001&units=imperial&APPID=bbeb34ebf60ad50f7893e7440a1e2b0b
+//URL: http://api.openweathermap.org/data/2.5/weather?zip=10001&units=imperial&APPID=_YOU_API_KEY_
 
 function zipUrl(zip) {
   return `${API_STEM}zip=${zip}&units=imperial&APPID=${WEATHER_API_KEY}`;
